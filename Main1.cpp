7e776142f8b0418e812d4b9c47c3316db09c066f
unknown <22chi.nh@vinuni.edu.vn>
2023-05-29 15:32:08 +0700

    Initial commit

---
+++ Main1.cpp
@@ -0,0 +1,286 @@
+#include<fstream>
+#include<conio.h>
+#include<stdio.h>
+#include<process.h>
+#include<string.h>
+#include<iomanip>
+#include<iostream>
+#include<vector>
+#include<list>
+using namespace std;
+
+class Book{
+    private :
+        string title;
+        string author;
+        string genre;
+        bool available;
+    public:
+        Book(string title, string author,string genre){
+            this-> title =title;
+            this -> author =author;
+            this -> genre =genre;
+        }
+        string getTitle() const {
+            return title;
+        }
+        string getAuthor() const {
+            return author;
+        }
+        string getGenre() const {
+            return genre;
+        };
+        bool operator==(const Book & other)const{    // the "const make sure that there is no change in attributes "
+            return (title==other.getTitle() && author== other.getAuthor());
+        }
+        bool operator<(const Book & other ) const{
+            if(title == other.getTitle()){
+                return author <other.getAuthor();
+            }
+            return (title <other.getTitle());
+        }
+        bool getAvailable(){
+            return available;
+        }
+        void Borrow_book(){
+            available =1;
+        }
+        void Return_book(){
+            available=0;
+        }
+        void get_information(){
+            cout << "The book :" << title << "belong to " << author << " is " << genre << "type book \n";
+        }
+};
+
+// This part is for split the information from file input purpose only
+struct information_book{
+    string title;
+    string author;
+    string genre;
+};
+//This function used to solve the space character in first and final of a string
+string trimSentence(string sentence) {
+    size_t start = sentence.find_first_not_of(" ");
+    size_t end = sentence.find_last_not_of(" ");
+
+    if (start == std::string::npos) {
+        // No non-space characters found, return an empty string
+        return "";
+    }
+
+    return sentence.substr(start, end - start + 1);
+};
+information_book read_line(string line){
+    string delimiter1="*Title:";
+    string delimiter2= "*Author:";
+    string delimiter3="*Genre:";
+    information_book book;
+    string genre;
+    string title;
+    string author;
+    size_t start = 0;
+    size_t end1 = line.find(delimiter1); // Find the position of the word delimiter
+    size_t end2 = line.find(delimiter2);
+    size_t end3= line.find(delimiter3);
+    title =line.substr(end1 + delimiter1.length(), end2 -delimiter2.length() );
+    author= line.substr(end2 + delimiter2.length(),end3 -end2-delimiter2.length());
+    genre=line.substr(end3 +delimiter3.length(), line.length() );     
+    book.title=trimSentence(title);
+    book.author=trimSentence(author);
+    book.genre=trimSentence(genre);
+    return book;
+}
+
+struct check_index{
+    int check_title;
+    int check_author;
+    int check_genre;
+};
+check_index hash_function(string title, string author, string genre){
+    check_index check_index;
+    trimSentence(title);
+    trimSentence(author);
+    trimSentence(genre);
+    int check_author1 =author[0];
+    int check_author2=author[author.length()/2];
+    int check_title1 =title[0];
+    int check_title2 =title[title.length()/2];
+    int check_genre1=genre[0];
+    int check_genre2=genre[genre.length()/2];
+    if ('A'<= check_author1 <='Z') check_author1 +=('a'-'A');
+    if ('A'<= check_author2 <='Z') check_author2 +=('a'-'A');
+    if ('A'<= check_title1 <='Z') check_title1 +=('a'-'A');
+    if ('A'<= check_title2 <='Z') check_title1 +=('a'-'A');
+    if ('A'<= check_genre1 <='Z') check_genre1 +=('a'-'A');
+    if ('A'<= check_genre2 <='Z') check_genre2 +=('a'-'A');
+    int check_author =check_author1+check_author2;
+    int check_title =check_title1 +check_title2;
+    int check_genre =check_genre1+check_genre2 ;
+    check_index.check_title=check_title;
+    check_index.check_author=check_author;
+    check_index.check_genre=check_genre; 
+    return check_index;
+}
+class Libary {
+    private:
+        vector<vector<vector<vector<Book>>>> ALL_BOOK;
+        void locate_the_book(string title ,string author,string genre,Book book){
+            check_index locate_book =hash_function(book.getTitle(), book.getAuthor(), book.getGenre());
+            cout << locate_book.check_genre <<" " << locate_book.check_title <<" "<< locate_book.check_author;
+            ALL_BOOK[locate_book.check_genre][locate_book.check_title][locate_book.check_author].push_back(book);
+        }
+    public :    
+        Libary(){
+            string title_name;
+            string author_name;
+            ifstream Data_base_book("book.inp");
+            if(Data_base_book.is_open()){
+                string line ;
+                while (getline(Data_base_book, line)){          
+                    information_book book =read_line(line);
+                    Book book1(book.title,book.author,book.genre);
+
+                    locate_the_book(book.title,book.author,book.genre,book1);
+                };
+                Data_base_book.close();
+            }else {
+            std::cerr << "Failed to open the file book.inp." << std::endl;
+            }
+        };
+        bool Find_the_book_availabel(string title,string author,string genre ){
+            // cout <<"in this step";
+            Book book_find(title,author,genre);
+            check_index index=hash_function(title, author, genre);
+            int check_title= index.check_title;
+            int check_author=index.check_author;
+            int check_genre=index.check_genre;
+            cout << check_title << check_author << check_genre;
+            Book book= ALL_BOOK[check_genre][check_title][check_author];
+            for(auto book : ALL_BOOK[check_genre][check_title][check_author]){
+                cout << "in this steps";
+                if(book== book_find){
+                    if(book.getAvailable()){
+                        cout << "The book :" << title << "of " << author << "is now available !";
+                        return 1; 
+                    }
+                } 
+            };
+            cout <<"The book :" << title << "of " << author << "is now not available !";
+            return 0;
+        }
+        // vector<Book> Find_book_with_special_info(string title="-1", string author="-1", string genre="-1"){
+        //     check_index index=hash_function(title, author, genre);
+        //     int check_title= index.check_title;
+        //     int check_author=index.check_author;
+        //     int check_genre=index.check_genre;
+        //     vector<Book> related_title;
+        //     vector<Book> related_author;
+        //     vector<Book> related_genre;
+        //     if(title=="-1"){
+        //         // if we do not have information about genre and title but have author informaion
+        //         if (genre =="-1"){
+        //             for(int i =0; i< ALL_BOOK.size(); i++){
+        //                 for (int j=0 ; j <ALL_BOOK[i].size(); j++){
+        //                     if (check_author < ALL_BOOK[i][j].size()){
+        //                         for(Book book : ALL_BOOK[i][j][check_author]){
+        //                             if(book.getAuthor()== author){
+        //                                 related_author.push_back(book);
+        //                             }
+        //                         }
+        //                     }  
+        //                 }
+        //             }
+        //         }
+        //         // if we do not have information about author and title but have genre information
+        //         else if (author=="-1"){
+        //             if (check_genre < ALL_BOOK.size()){
+        //                 for(int i=0; i<ALL_BOOK[check_genre].size(); i++){
+        //                     for(int j= 0; j< ALL_BOOK[check_genre][i].size(); j++){
+        //                         for(Book book: ALL_BOOK[check_genre][i][j]){
+        //                             if(book.getGenre()== genre){
+        //                                 related_genre.push_back(book);
+        //                             }
+        //                         }
+        //                     }
+        //                 }
+        //             }
+                    
+        //         }// if we have information about genre and author but not have information of title  
+        //         else {
+        //             if(check_genre < ALL_BOOK.size()){
+        //                 for (int i=0; i<ALL_BOOK[check_genre].size(); i++ ){
+        //                     if (check_author< ALL_BOOK[check_author][i].size()){
+        //                         for(Book book : ALL_BOOK[check_genre][i][check_author]){
+        //                             if(book.getAuthor()== author and book.getGenre()== genre){
+        //                                 related_author.push_back(book);
+        //                             }
+        //                         }
+        //                     }
+        //                 }
+        //             } 
+        //         }                
+        //     }
+        //     // if we have information of title
+        //     if(author=="-1" && title!="-1"){
+        //         // if we do not have information of genre
+        //         if (genre =="-1"){
+        //             for(int i=0; i < ALL_BOOK.size(); i++){
+        //                 if (check_title <ALL_BOOK[i].size()){
+        //                     for(int j=0; j< ALL_BOOK[i][check_title].size(); j++){
+        //                         for(Book book : ALL_BOOK[i][check_title][j]){
+        //                             if (book.getTitle()==title){
+        //                                 related_title.push_back(book);
+        //                             }
+        //                         }
+        //                     }
+        //                 } 
+        //             }
+        //         }
+        //         // if we have information of genre
+        //         else{
+        //             if (check_genre < ALL_BOOK.size() && check_title< ALL_BOOK[check_genre].size()){
+        //                 for(int i =0; i < ALL_BOOK[check_genre][check_title].size(); i++){
+        //                     for(Book book : ALL_BOOK[check_genre][check_title][i]){
+        //                         if(book.getTitle()==title && book.getGenre()==genre){
+        //                             related_title.push_back(book);
+        //                         }
+        //                     }
+        //                 }
+        //             } 
+        //         }
+        //     }
+
+        //     // print out the available book;
+        //     if (related_title.size()!=0){
+        //         return related_title;
+        //         cout << "List of books whose title is "<< title << "are:\n";
+        //         for (Book book : related_title){
+        //             book.get_information();
+        //         }
+        //     } else if (related_author.size()!= 0){
+        //         return related_author;
+        //         cout <<"List of books whose author is "<< author <<"are :\n";
+        //         for (Book book : related_author){
+        //             book.get_information();
+        //         }
+        //     } else if(related_genre.size()!=0){
+        //         return related_genre;
+        //         cout<< "List of books whose genre is "<< genre << "are :\n";
+        //         for(Book book : related_genre){
+        //             book.get_information();
+        //         }
+        //     } else{
+        //         cout<< "Can not find any book";
+        //         return related_author;
+        //     }
+        // } 
+};
+int main() {
+    Libary Libary;
+    // cout << "which book do you want to find :";
+    // string author_in ;
+    // getline(cin, author_in);
+    Libary.Find_the_book_availabel("To Kill a Mockingbird" ," Harper Lee "," Fiction");
+    return 0;
+}
---
+++ smalltest.cpp
@@ -0,0 +1,16 @@
+#include<fstream>
+#include<conio.h>
+#include<stdio.h>
+#include<process.h>
+#include<string>
+#include<iomanip>
+#include<iostream>
+#include<vector>
+#include<list>
+using namespace std;
+
+int main(){
+    string a;
+    getline(cin,a);
+    cout <<a ;
+}
